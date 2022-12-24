f0216344b3b4b67d72979d64f2b14161644cf280
Xharlez47 <charleskelvin47@gamil.com>
2022-12-24 10:32:30 +0000

    more data structures

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,5 @@
-#!/usr/python3
+#!/usr/bin/python3
 def best_score(a_dictionary):
     if len(a_dictionary) == 0):
-        return None
+       return None
     return max(a_dictionary, key=a_dictionary.get)
