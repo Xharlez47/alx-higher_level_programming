5b016f7f38a9e9c83441d92ae78830c7c12eac7c
Xharlez47 <charleskelvin47@gamil.com>
2022-12-27 16:50:46 +0000

    OOP

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""defines a class a Square and has a private instance attribute called size"""
+
+
+class Square:
+    """A class square with size atrribute
+    Attributes:
+        __size (int): size of the square
+    """
+    def __init__(self, size=0):
+        """initializes a Square
+        Args:
+            size (int): size of a side of the square
+        Returns: None
+        """
+        """to ensure size is an integer"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """calculates the area of the square which is size ** 2
+        Returns:
+            area of the square
+        """
+        return (self.__size) ** 2
+
+    @property
+    def size(self):
+        """a getter allows us to refers to the fields
+        Returns: the size of the square
+        """
+    return self.__size
+    @size.setter
+    def size(self, value):
+        """a setter allows to input correct values like needed"""
+        if value.isdigit():
+            self.__size = value
