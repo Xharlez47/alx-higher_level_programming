81f70cd02ec590d7a26267a2e1fe3bff2b34b684
Xharlez47 <charleskelvin47@gamil.com>
2022-12-27 16:33:58 +0000

    OOP

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -19,6 +19,7 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         """calculates the area of the square which is size ** 2
         Returns:
