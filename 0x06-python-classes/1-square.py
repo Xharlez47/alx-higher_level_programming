767ad87328b6163a18c87715f74054f5e9cda2b1
Xharlez47 <charleskelvin47@gamil.com>
2022-12-27 15:40:14 +0000

    OOP

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,7 +3,12 @@
 
 
 class Square:
-    """A class square with size atrribute"""
-    pass
-    def __init__(self, size =0)
-    size.__size = 0 
+    """A class square with size atrribute
+       Attributes: __size (int): size of the square """
+    def __init__(self, size)
+        """initializes a Square
+        Args: size
+
+        Returns: none
+        """
+        size.__size = size
