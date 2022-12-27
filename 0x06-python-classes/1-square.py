126f7f4f7601994c22eff975021d85eea26d5271
Xharlez47 <charleskelvin47@gamil.com>
2022-12-27 15:24:30 +0000

    OOP

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""defines a class a Square and has a private instance attribute called size"""
+
+
+class Square:
+    """A class square with size atrribute"""
+    pass
+    def __init__(self, size=0):
