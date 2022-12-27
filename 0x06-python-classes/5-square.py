df822d52e9a6159efe32a1b0dd8feecf83e247b6
Xharlez47 <charleskelvin47@gamil.com>
2022-12-27 17:36:45 +0000

    OOP

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""defines a class a Square and has a private instance attribute called size"""
+
+
+class Square:
+    """A class square with size atrribute
+    Attributes:
+        __size (int): size of the square
+    """
+    def __init__(self, size=0):
+        """initializes a Square
+        Args:
+            size (int): size of a side of the square
+        Returns: None
+        """
+        """to ensure size is an integer"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """calculates the area of the square which is size ** 2
+        Returns:
+            area of the square
+        """
+        return (self.__size) ** 2
+
+    @property
+    def size(self):
+        """a getter allows us to refers to the fields
+        Returns: the size of the square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """a setter allows to input correct values like needed"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+    def my_print(self):
+        """"print the square with # to stdout"""
+        if size > 0:
+            for i in range(1, value)
+                for j in range(1, value)
+                    print("#", end="")
+                print()
