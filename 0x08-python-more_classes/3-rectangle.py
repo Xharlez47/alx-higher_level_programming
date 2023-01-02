61e626d7809651e3e3943250ec3dff2861fd15fe
Xharlez47 <charleskelvin47@gamil.com>
2023-01-02 21:04:35 +0000

    more classes

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -63,8 +63,9 @@ class Rectangle:
         return (self.__width * 2) + (self.__height * 2)
 
     def __str__(self):
-        """print the Rectangle with # to stdout"""
+        """returns printable string representation of the rectangle"""
         string = ""
         if self.__width != 0 and self.__height != 0:
-            string += "\n".join("#" * self.__width for i in range(self.__height))
+            string += "\n".join("#" * self.__width
+                                for j in range(self.__height))
         return string
