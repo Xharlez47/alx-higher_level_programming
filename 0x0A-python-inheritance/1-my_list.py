e4e7d2848ff8e7a98e7894ac64fd0921aad889d3
Xharlez47 <charleskelvin47@gamil.com>
2023-01-10 09:53:41 +0000

    inheritnace

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/uxr/bin/python3
+"""
+    a class Mylist
+"""
+
+
+    class Mylist(list):
+        """inherits the class list"""
+        def __init__(self):
+            """initializes the object"""
+            super().__init__()
+
+        def print_Sorted(self):
+            """prints the sorted list"""
+            print(sorted(self))
